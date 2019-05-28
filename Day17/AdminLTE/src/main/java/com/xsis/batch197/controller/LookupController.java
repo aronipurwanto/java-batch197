d5d55b0404220e9d78ace6195ba6c774a4c4b754
ahmadroni <ahmadroni@gmail.com>
2019-05-28 14:29:10 +0700

    update lookup

--- Day17/AdminLTE/src/main/java/com/xsis/batch197/controller/LookupController.java
+++ Day17/AdminLTE/src/main/java/com/xsis/batch197/controller/LookupController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -54,7 +55,7 @@ public class LookupController {
 	public ModelAndView save(@Valid @ModelAttribute("lookup") LookupModel lookup, BindingResult result) {
 		// buat object view
 		ModelAndView view = new ModelAndView();
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			logger.info("save lookup error");
 			view.setViewName("lookup/create");
 			view.addObject("lookup", lookup);
@@ -62,6 +63,75 @@ public class LookupController {
 		}
 		// jika tidak ada error
 		repo.save(lookup);
+		view.setViewName("lookup/index");
+		// redirect to index
+		return new ModelAndView("redirect:/lookup/index");
+	}
+
+	// #2. Membuat Form Add lookup
+	@GetMapping(value = "/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		// buat object view
+		ModelAndView view = new ModelAndView("lookup/update");
+		// mengambil data dahulu dari database via repository
+		LookupModel lookup = repo.findById(id).orElse(new LookupModel());
+		// membuat object lookup yg akan dikirim ke view
+		// object lookup adalah new object dari LookupModel
+		view.addObject("lookup", lookup);
+		return view;
+	}
+
+	// #3. Menangkap data dari form
+	@PostMapping(value = "/update")
+	public ModelAndView update(@Valid @ModelAttribute("lookup") LookupModel lookup, BindingResult result) {
+		// buat object view
+		ModelAndView view = new ModelAndView();
+		if (result.hasErrors()) {
+			logger.info("save lookup error");
+			view.setViewName("lookup/update");
+			view.addObject("lookup", lookup);
+			return view;
+		}
+		// jika tidak ada error
+		repo.save(lookup);
+		view.setViewName("lookup/index");
+		// redirect to index
+		return new ModelAndView("redirect:/lookup/index");
+	}
+
+	// #4. Membuat Form Add lookup
+	@GetMapping(value = "/detail/{id}")
+	public ModelAndView detail(@PathVariable("id") Long id) {
+		// buat object view
+		ModelAndView view = new ModelAndView("lookup/detail");
+		// mengambil data dahulu dari database via repository
+		LookupModel lookup = repo.findById(id).orElse(new LookupModel());
+		// membuat object lookup yg akan dikirim ke view
+		// object lookup adalah new object dari LookupModel
+		view.addObject("lookup", lookup);
+		return view;
+	}
+
+	// #4. Membuat Form Add lookup
+	@GetMapping(value = "/delete/{id}")
+	public ModelAndView delete(@PathVariable("id") Long id) {
+		// buat object view
+		ModelAndView view = new ModelAndView("lookup/delete");
+		// mengambil data dahulu dari database via repository
+		LookupModel lookup = repo.findById(id).orElse(new LookupModel());
+		// membuat object lookup yg akan dikirim ke view
+		// object lookup adalah new object dari LookupModel
+		view.addObject("lookup", lookup);
+		return view;
+	}
+
+	// #3. Menangkap data dari form
+	@PostMapping(value = "/remove")
+	public ModelAndView remove(@ModelAttribute("lookup") LookupModel lookup) {
+		// buat object view
+		ModelAndView view = new ModelAndView();
+		// jika tidak ada error
+		repo.delete(lookup);
 		view.setViewName("lookup/index");
 		// redirect to index
 		return new ModelAndView("redirect:/lookup/index");
