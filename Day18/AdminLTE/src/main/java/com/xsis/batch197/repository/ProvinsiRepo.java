2fdc3fbb73105cd82b87231447f8ccd476b55edb
ahmadroni <ahmadroni@gmail.com>
2019-06-10 13:06:34 +0700

    update Day 19

--- Day18/AdminLTE/src/main/java/com/xsis/batch197/repository/ProvinsiRepo.java
+++ Day18/AdminLTE/src/main/java/com/xsis/batch197/repository/ProvinsiRepo.java
@@ -1,7 +1,10 @@
 package com.xsis.batch197.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.xsis.batch197.model.ProvinsiModel;
@@ -11,4 +14,9 @@ public interface ProvinsiRepo extends JpaRepository<ProvinsiModel, Long> {
 	
 	@Query(value="select max(kdProvinsi) from ProvinsiModel")
 	public String getMaxKode();
+	
+	public List<ProvinsiModel> findByNama(String nmProvinsi);
+	
+	@Query(value="SELECT p FROM ProvinsiModel p WHERE LOWER(p.nmProvinsi) = LOWER(:nama) ")
+	public List<ProvinsiModel> search(@Param("nama") String nama);
 }
---
+++ Day19/AdminLTE/src/main/java/com/xsis/batch197/repository/BiodataRepo.java
@@ -0,0 +1,18 @@
+package com.xsis.batch197.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.xsis.batch197.model.BiodataModel;
+
+@Repository
+public interface BiodataRepo extends JpaRepository<BiodataModel, Long> {
+	public List<BiodataModel> findByNik(String nik);
+	
+	@Query(value="SELECT b FROM BiodataModel WHERE LOWER(nik) LIKE CONCAT('%',LOWER(:nama),'%')")
+	public List<BiodataModel> search(@Param("nama") String nama);
+}
